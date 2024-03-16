63897d420a7ad3a13f40b25f93221159087078ad
Jacliscs <2628405129@qq.com>
2024-03-16 18:46:32 +0800

    OD344 找座位

--- src/OD344/Main.java
+++ src/OD344/Main.java
@@ -25,7 +25,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         while (sc.hasNext()) {
             String seat = sc.nextLine();
             System.out.println(maxSeat(seat));
