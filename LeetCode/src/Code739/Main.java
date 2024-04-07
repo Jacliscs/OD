d06645096984d8c50757115b16a5841575af224e
Jacliscs <2628405129@qq.com>
2024-04-07 18:20:23 +0800

    LeetCode739 每日温度 [优化]

--- LeetCode/src/Code739/Main.java
+++ LeetCode/src/Code739/Main.java
@@ -22,21 +22,21 @@ public class Main {
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
         int n = temperatures.length;
-
         //结果
         int[] ans = new int[n];
-
-        //暴力
-        for (int i = 0; i < n; i++) {
-            //如果后面没有温度比自己高的，则ans[i]=0
-            int res = 0;
-            for (int j = i + 1; j < n; j++) {
+        //从右往左遍历
+        for (int i = n - 2; i >= 0; i--) {
+            //ans[j]表示下一个比自己大的数间隔了几位，每次都跳转到下一个比自己大的位置，减少循环次数
+            for (int j = i + 1; j < n; j += ans[j]) {
                 if (temperatures[j] > temperatures[i]) {
-                    res = j - i;
+                    ans[i] = j - i;
+                    break;
+                } else if (ans[j] == 0) {
+                    //后面没有比ans[j]大的数了
+                    ans[i] = 0;
                     break;
                 }
             }
-            ans[i] = res;
         }
         return ans;
     }
