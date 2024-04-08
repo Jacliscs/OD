f558f9b2d41478f7b19de0c4c0511315fb13b5f1
Jacliscs <2628405129@qq.com>
2024-04-08 17:20:34 +0800

    OD242 靠谱的车 [100][数学问题、进制转换][优化注释]

--- src/OD242/Main.java
+++ src/OD242/Main.java
@@ -6,12 +6,20 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        //计费表的表面读数
         long n = sc.nextLong();
+        //实际上是按九进制进位的，所以拆分
         long[] arr = Arrays.stream(String.valueOf(n).split("")).mapToLong(Long::parseLong).toArray();
 
         System.out.println(getResult(arr));
     }
 
+    /**
+     * 实际上是将九进制数转为十进制数
+     *
+     * @param arr
+     * @return long
+     */
     public static long getResult(long[] arr) {
         int correct = 0;
 
