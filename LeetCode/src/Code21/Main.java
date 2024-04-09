dd09ff3bd3518b6ca7f03924d9f2aac2e0c337bb
Jacliscs <2628405129@qq.com>
2024-04-09 20:11:54 +0800

    LeetCode21 合并两个有序链表 [简单][递归]

---
+++ LeetCode/src/Code21/Main.java
@@ -0,0 +1,67 @@
+package Code21;
+
+import java.util.Scanner;
+
+/**
+ * @author 浮生
+ * @description 合并两个有序链表
+ * @date 2024/4/9
+ * @level 简单
+ * @score
+ * @url https://leetcode.cn/problems/merge-two-sorted-lists/
+ */
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        //默认升序
+        if (list1 == null) {
+            return list2;
+        } else if (list2 == null) {
+            return list1;
+        } else if (list1.val < list2.val) {
+            //把头结点提出来，把剩下的递归升序排列
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        } else {
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
