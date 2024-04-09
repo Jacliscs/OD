334952aca6a047c0d640849e4188f2537af9f41c
Jacliscs <2628405129@qq.com>
2024-04-09 19:39:20 +0800

    LeetCode594 最长和谐子序列 [简单][双指针优化效率]

--- LeetCode/src/Code594/Main.java
+++ LeetCode/src/Code594/Main.java
@@ -25,22 +25,32 @@ class Solution {
     public int findLHS(int[] nums) {
         //先排序
         Arrays.sort(nums);
-        //存放每个数字出现的次数
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-
         //题解：最长和谐子序列的长度
         int ans = 0;
-        for (int key : map.keySet()) {
-            //如果存在刚好比key大1的数
-            if (map.containsKey(key + 1)) {
-                //最长和谐子序列的长度就是key key+1的次数和
-                ans = Math.max(ans, map.get(key) + map.get(key + 1));
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            //left是较小数字的左边界 right是较大数字的右边届
+            while (nums[right] - nums[left] > 1) {
+                left++;
+            }
+
+            //如果两个数字差刚好为1
+            if (nums[right] - nums[left] == 1) {
+                ans = Math.max(ans, right - left + 1);
             }
         }
+        //存放每个数字出现的次数
+        //HashMap<Integer, Integer> map = new HashMap<>();
+        //for (int i = 0; i < nums.length; i++) {
+        //    map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        //}
+        //for (int key : map.keySet()) {
+        //    //如果存在刚好比key大1的数
+        //    if (map.containsKey(key + 1)) {
+        //        //最长和谐子序列的长度就是key key+1的次数和
+        //        ans = Math.max(ans, map.get(key) + map.get(key + 1));
+        //    }
+        //}
         return ans;
-
     }
 }
