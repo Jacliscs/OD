fcf6823f03d0ccbefc336f1c6363ed0a6e32756c
Jacliscs <2628405129@qq.com>
2024-04-09 19:36:03 +0800

    LeetCode594 最长和谐子序列 [简单][哈希表、效率不高]

---
+++ LeetCode/src/Code594/Main.java
@@ -0,0 +1,46 @@
+package Code594;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * @author 浮生
+ * @description 最长和谐子序列
+ * @date 2024/4/9
+ * @level 简单
+ * @score
+ * @url https://leetcode.cn/problems/longest-harmonious-subsequence/description/
+ */
+// 注意类名必须为 Main, 不要有任何 package xxx 信息
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+    }
+}
+
+class Solution {
+    public int findLHS(int[] nums) {
+        //先排序
+        Arrays.sort(nums);
+        //存放每个数字出现的次数
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        //题解：最长和谐子序列的长度
+        int ans = 0;
+        for (int key : map.keySet()) {
+            //如果存在刚好比key大1的数
+            if (map.containsKey(key + 1)) {
+                //最长和谐子序列的长度就是key key+1的次数和
+                ans = Math.max(ans, map.get(key) + map.get(key + 1));
+            }
+        }
+        return ans;
+
+    }
+}
